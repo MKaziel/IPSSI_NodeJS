63d3d97c582899d678470997e8c771caf07067cb
pointk <pointk@geeps.centralesupelec.fr>
2020-11-24 11:30:01 +0100

    All function complete

--- src/api/controllers/commentController.js
+++ src/api/controllers/commentController.js
@@ -37,11 +37,41 @@ exports.create_a_comment = (req, res) =>{
 }
 
 exports.get_a_comment = (req,res) => {
-
+    Comment.findById(req.params.comment_id, (error,comment) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            res.status(200);
+            console.log(comment);
+            res.json({
+                message: "Comment find !",
+                object : comment
+            });
+        }
+    })
 }
 
 exports.update_a_comment = (req,res) => {
-
+    Comment.findByIdAndUpdate(req.params.comment_id,req.body,{new:true},(error,comment) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            res.status(200);
+            console.log(comment);
+            res.json({
+                message: "Comment modified !",
+                object : comment
+            });
+        }
+    })
 }
 
 exports.delete_a_comment = (req,res) => {
